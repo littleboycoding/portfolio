9d04e9807b91e70dd3ff103b412f23fe72edb0a6
Thanawat Yodnil <littleboycoding@gmail.com>
2022-08-11 16:24:38 +0700

    Updates

--- _next/static/Pze8ILyopygjJrcvDg5_-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/848-7600ddda5b2a76b0.js","static/chunks/pages/index-d544111a6e184ed2.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
