4314d31a2cec8b658ea0dafd52330f49219b9df0
Thanawat Yodnil <littleboycoding@gmail.com>
2022-08-11 20:38:00 +0700

    Added unoptimized config to next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   basePath: "/portfolio",
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
 };
 
 module.exports = nextConfig;
